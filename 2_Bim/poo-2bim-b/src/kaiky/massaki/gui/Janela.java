dccdf32c9aa015c56e9afbf1c5f128d47aa52194
Kaiky M. Ishibashi <112902634+ShibaDX@users.noreply.github.com>
2025-05-23 22:21:09 -0300

    aula 23-05

--- 2_Bim/poo-2bim-b/src/kaiky/massaki/gui/Janela.java
+++ 2_Bim/poo-2bim-b/src/kaiky/massaki/gui/Janela.java
@@ -1,43 +1,71 @@
 package kaiky.massaki.gui;
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Janela extends JFrame{
+public class Janela extends JFrame {
 
     private JLabel jlNome;
     private JTextField tfNome;
-
     private JLabel jlSobrenome;
     private JTextField tfSobrenome;
-
+    private JLabel jlEndereco;
+    private JTextField tfEndereco;
     private JButton btConfirmar;
+    private JButton btListar;
 
-    public Janela(){
+    public Janela() {
         setTitle("Cadastro de Aluno");
-        setSize(400,400);
+        setSize(400, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         var jPanel = new JPanel(new GridBagLayout());
 
         jlNome = new JLabel("Nome");
-        jPanel.add(jlNome, montarConstraints(0,0));
+        jPanel.add(jlNome, montarConstraints(0, 0));
         tfNome = new JTextField(20);
-        jPanel.add(tfNome, montarConstraints(1,0));
+        jPanel.add(tfNome, montarConstraints(1, 0));
 
         jlSobrenome = new JLabel("Sobrenome");
-        jPanel.add(jlSobrenome, montarConstraints(0,1));
+        jPanel.add(jlSobrenome, montarConstraints(0, 1));
         tfSobrenome = new JTextField(20);
-        jPanel.add(tfSobrenome, montarConstraints(1,1));
+        jPanel.add(tfSobrenome, montarConstraints(1, 1));
+
+        jlEndereco = new JLabel("Endereço");
+        jPanel.add(jlEndereco, montarConstraints(0,2));
+        tfEndereco = new JTextField(20);
+        jPanel.add(tfEndereco, montarConstraints(1,2));
 
         btConfirmar = new JButton("Confirmar");
         btConfirmar.addActionListener(e -> confirmar(e));
-        jPanel.add(btConfirmar,montarConstraints(1,2));
+        jPanel.add(btConfirmar, montarConstraints(0, 3));
+
+        btListar = new JButton("Listar Registros");
+        btListar.addActionListener(this::listarRegistros);
+        jPanel.add(btListar, montarConstraints(1, 3));
 
         add(jPanel);
     }
 
-    private boolean isBlank(JTextField campo, String mensagem) {
+    private void listarRegistros(ActionEvent event) {
+        var arquivo = new File(
+                System.getProperty("user.dir"),
+                "\\arquivo.txt");
+        var registros = readerFile(arquivo.toString());
+
+        var message = "";
+        for (String registro: registros){
+            message = message + "\n" + registro;
+        }
+
+        JOptionPane.showMessageDialog(this, message);
+    }
+
+    private Boolean isBlank(JTextField campo, String mensagem) {
         if (campo.getText().isBlank()) {
             JOptionPane.showMessageDialog(this, mensagem);
             return Boolean.TRUE;
@@ -46,22 +74,48 @@ public class Janela extends JFrame{
     }
 
     private void confirmar(ActionEvent event) {
+        if (isBlank(tfNome, "Informe o Nome!")) return;
+        if (isBlank(tfSobrenome, "Informe o Sobrenome!")) return;
+        if (isBlank(tfEndereco, "Informe o Endereço!")) return;
 
-        if(isBlank(tfNome, "Informe o nome!")) return;
-        if(isBlank(tfSobrenome, "Informe o sobrenome!")) return;
+        System.out.println("Salvando no Arquivo.txt");
 
-        System.out.println("Salvando no Banco de Dados");
-        System.out.println("Nome: " + tfNome.getText());
-        System.out.println("Sobrenome: " + tfSobrenome.getText());
+        var texto = "Nome: " + tfNome.getText() +
+                "- Sobrenome: " + tfSobrenome.getText() +
+                "\nEndereço: " + tfEndereco.getText();
 
+        writerFile(texto, new File(
+                System.getProperty("user.dir"),
+                "\\arquivo.txt")
+                .toString());
     }
 
-    private GridBagConstraints montarConstraints(int x, int y){
+    private GridBagConstraints montarConstraints(int x, int y) {
         var constraint = new GridBagConstraints();
-        constraint.insets = new Insets(5,5,5,5);
+        constraint.insets = new Insets(5, 5, 5, 5);
         constraint.gridx = x;
         constraint.gridy = y;
         return constraint;
+    }
+
+    private void writerFile(String conteudo, String nomeArquivo) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo, true))) {
+            writer.newLine();
+            writer.write(conteudo);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private List<String> readerFile(String nomeArquivo) {
+        List<String> registros = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(nomeArquivo))) {
+            reader.lines().forEach(registros::add);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return registros;
     }
 
     public static void main(String[] args) {
